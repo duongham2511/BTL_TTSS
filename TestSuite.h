387a05e06a37f0b713b7f02d0c94f3e88b050aa4
duong <duong.vu2511@hcmut.edu.vn>
2021-06-12 17:29:14 +0700

    add another alternative Strassen parallel

--- TestSuite.h
+++ TestSuite.h
@@ -282,6 +282,61 @@ class TestSuite{
         Utility::FreeMemory2D<double>(A);
         Utility::FreeMemory2D<double>(B);
     }
+
+    static void StrassenOMP_03Test(int rowA, int colA_rowB, int colB, int attempts)
+    {
+        double **A = Utility::AllocateMemory2D<double>(rowA,colA_rowB);
+        double **B = Utility::AllocateMemory2D<double>(colA_rowB,colB);
+        double **C;
+        double start, end;
+        TestSuite::GenTestMat(A,B,rowA,colA_rowB,colB);
+
+        printf("%d threads available for parallel processing.\n",omp_get_max_threads());
+        printf("Multiplying a %d x %d matrix with a %d x %d matrix using Strassen OMP alt 2, %d try.\n",rowA, colA_rowB, colA_rowB, colB, attempts);
+
+        int max = fmax(rowA,colB);
+        max = max > colA_rowB ? max : colA_rowB;
+
+        int n = Utility::getPower2(max);
+        bool needPad = !((n == max) && (rowA == colA_rowB) && (colA_rowB == colB));
+        if  (needPad)
+        {
+            printf("Matrix A of %d x %d padded to %d x %d.\n", rowA, colA_rowB, n, n);
+            MatOp::pad2D(A,rowA,colA_rowB,n,n);
+            // Utility::printMat(A,n,n);
+            printf("Matrix B of %d x %d padded to %d x %d.\n", colA_rowB, colB, n, n);
+            MatOp::pad2D(B,colA_rowB,colB,n,n);
+            // Utility::printMat(B,n,n);
+        }
+
+        for (int i =1; i <= attempts; i++)
+        {
+            printf("Attempt %-2d starting...    ", i);
+            C = Utility::AllocateMemory2D<double>(n,n);
+            start = omp_get_wtime();
+            matMul_Strassen_P3(A,B,C,n);
+            end = omp_get_wtime();
+
+            if (needPad)
+            {
+                // printf("Matrix C of %d x %d shrunk to %d x %d.\n", n, n, rowA, colB);
+                MatOp::shrink2D<double>(C,n,n,rowA,colB);
+                // Utility::printMat(C,rowA,colB);
+            }
+
+            if (TestSuite::TestProduct(C,rowA,colA_rowB,colB))
+                printf("Attempt %-2d successful. Runtime is %3.2f seconds.\n",i,end - start);
+            else
+            {
+                printf("Attempt %-2d unsuccessful. Ending test...\n",i);
+                break;
+            }
+            Utility::FreeMemory2D<double>(C);
+        }
+
+        Utility::FreeMemory2D<double>(A);
+        Utility::FreeMemory2D<double>(B);
+    }
 };
 
 #endif
--- main.cpp
+++ main.cpp
@@ -11,7 +11,8 @@
 int main(int argc, char **argv)
 {
     omp_set_num_threads(32);
-    TestSuite::StrassenOMP_02Test(2048,2048,2048,3);
-    TestSuite::StrassenOMP_01Test(2048,2048,2048,3);
+    TestSuite::StrassenOMP_02Test(4096,4096,4096,5);
+    TestSuite::StrassenOMP_01Test(4096,4096,4096,5);
+    TestSuite::StrassenOMP_03Test(4096,4096,4096,5);
     return 0;
 }
